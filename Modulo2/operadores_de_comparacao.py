e7a882fc400eca2208467f925be43fbbb9cc307b
helio212 <heliosol777@gmail.com>
2024-06-06 11:24:08 -0300

    Anotações sobre operadores de comparação adicionada.

---
+++ Modulo2/operadores_de_comparacao.py
@@ -0,0 +1,39 @@
+
+#igualdade: =/== 
+
+saldo = 450
+saque = 200
+
+print("saldo é igual a saque?")
+print(saldo == saque)
+#saldo é igual a saque?
+#--> falso
+
+#Diferente: !=
+
+print("saldo é diferente de saque?")
+print(saldo != saque)
+#saldo é diferente de saque?
+#--> verdadeiro
+
+
+#maior que/maior ou igual:
+
+#maior que
+print("saldo é maior que saque?")
+print(saldo > saque)
+
+#maior ou igual
+print("saldo é maior ou igual a saque?")
+print(saldo >= saque)
+
+
+#menor que/menor ou igual:
+
+#menor que
+print("saldo é menor que saque?")
+print(saldo < saque)
+
+#menor ou igual
+print("saldo é menor ou igual a saque?")
+print(saldo <= saque)
