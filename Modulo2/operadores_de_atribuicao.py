95813a0efe9c2cab3e6c073eb748b2a23134f062
helio212 <heliosol777@gmail.com>
2024-06-06 11:39:01 -0300

    anotações sobre operadores de atribuicao adicionadas.

---
+++ Modulo2/operadores_de_atribuicao.py
@@ -0,0 +1,36 @@
+# tipos de atribuições:
+
+#simples:
+saldo = 500
+
+#o valor de 500 foi atribuido à 500.
+
+#atribuição de adição
+saldo += 200
+
+
+#atribuição de subtração:
+saldo1 = 200
+saldo1 -= 200
+
+#divisão:
+
+saldo2 = 200
+saldo2 /= 5
+
+#multiplicação:
+
+saldo3 = 200
+saldo3 *= 2
+
+#divisão inteira
+saldo4 = 200
+saldo4 //= 5
+
+#modular:
+saldo5 = 200
+saldo5 %=480
+
+#exponencial:
+saldo6 = 200
+saldo6 **= 2
