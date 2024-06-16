19cb4b0fe092535453b386caa515b6612dc52e79
heliosol212 <heliosol777@gmail.com>
2024-06-15 23:49:50 -0300

    arquivo py modulo 2 atualizado

--- Modulo2/estruturas_condicionais_repeticao/repeticao_teste.py
+++ Modulo2/estruturas_condicionais_repeticao/repeticao_teste.py
@@ -1,14 +1,13 @@
 #Definir o numero de inputs (entradas):
 def capturar_valores():
-    nde = input("Quantas entradas você quer?")
+    nde = int(input("Quantas entradas você quer?"))
     entradas = []
 
     for i in range(nde):
-        entrada = input(f"Digite o valor {i+1}: ")
-        entradas.append(valor)
+        entrada = int(input(f"Digite o valor {i+1}: "))
+        entradas.append(entrada)
     return entradas
 
 lista_valores = capturar_valores()
 
 print("Valores inseridos:", lista_valores)
-
